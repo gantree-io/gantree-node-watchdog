3d14fa4503eeca0ae3e843519b2f2bc0428ef98f
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-16 12:50:53 +1000

    add register function

--- gantree_node_watchdog/script.py
+++ gantree_node_watchdog/script.py
@@ -6,6 +6,14 @@ from .environment import get_env_vars
 env = get_env_vars()
 
 
+def register(hostname, api_key, project_id, ip_address):
+    return requests.post(
+        f"{hostname}/clientNode/register",
+        headers={"Authorization": f"Api-Key {api_key}"},
+        json={"projectId": project_id, "ipAddress": ip_address},
+    )
+
+
 def proxy_scrape(hostname, node_secret):
     return requests.post(
         f"{hostname}/clientNode/proxyScrape",
