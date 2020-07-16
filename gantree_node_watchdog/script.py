6d9b9dcd31479d29cd6ef35925b280e79894212f
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-16 12:34:53 +1000

    get environment variables for sensitive data

--- gantree_node_watchdog/script.py
+++ gantree_node_watchdog/script.py
@@ -1,4 +1,12 @@
 import requests
+import os
+
+HOSTNAME = os.getenv("GANTREE_NODE_WATCHDOG_HOSTNAME")
+API_KEY = os.getenv("GANTREE_NODE_WATCHDOG_API_KEY")
+PROJECT_ID = os.getenv("GANTREE_NODE_WATCHDOG_PROJECT_ID")
+IP_ADDRESS = os.getenv("GANTREE_NODE_WATCHDOG_IP_ADDRESS")
+NODE_ID = os.getenv("GANTREE_NODE_WATCHDOG_NODE_ID")
+NODE_SECRET = os.getenv("GANTREE_NODE_WATCHDOG_NODE_SECRET")
 
 def main():
     response = requests.get('http://127.0.0.1:9615/metrics')
