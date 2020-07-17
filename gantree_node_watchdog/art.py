223f2bad2e9ae532063a55346611cbf12a016a7c
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-17 15:28:12 +1000

    add WIP version of new logotype (unused)

--- gantree_node_watchdog/art.py
+++ gantree_node_watchdog/art.py
@@ -9,3 +9,18 @@ gantree_art = "\n\
  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝\n\
 "
 
+new_gantree_art = "\n\
+Moooooooooooooooooods                                                           \n\
+N                  ys                                                           \n\
+MysssssssdmssssssssyysssssssssssssssssssssssssssssssssssssssssssssssssssssssssyN\n\
+N        +h                                                                   .M\n\
+N        +d`                                                                  .M\n\
+N     `+ysoosy:      `dh`    .Ns    -d  ossNyso  hhssss.  -mssss/  -mssss:    .M\n\
+N    .d/       .     h/sy    .Nod.  -N    `M-    m+   sh  -M`      :N         .M\n\
+N    oh   -osssN:   ss `d+   .N :d/ -N    `M-    my++sd:  -Mssss-  :Mssss-    .M\n\
+N    :N.  ``  `N-  +NsssyN:  .N  `yy:N    `M-    mo.sd.   -M`      :N         .M\n\
+N     /do-``.+h/  :N.    :N. .N    +mN    `M-    m+  :m+  -M:---.  :M----.    .M\n\
+N       -+oo+-    :.      :- `/     .:     /`    :.   `/. `/////-  `/////-    .M\n\
+N                                                                             .M\n\
+MsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssyM\n\
+"
