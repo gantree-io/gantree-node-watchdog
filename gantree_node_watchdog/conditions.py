61fa76faaa1c1d80a30923903b6ba99fcf59ea5b
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-18 00:27:58 +1000

    docstrings for funcs in conditions

--- gantree_node_watchdog/conditions.py
+++ gantree_node_watchdog/conditions.py
@@ -1,12 +1,18 @@
 def is_exception(item):
+    """Self-explanatory."""
     return True if isinstance(item, Exception) else False
 
 
 def is_false(item):
+    """Self-explanatory."""
     return True if item is False else False
 
 
 def is_409(item):
+    """Return True if the item has a status_code of 409.
+    
+    Return TypeError if item doesn't have a status_code.
+    """
     if hasattr(item, "status_code"):
         return True if item.status_code == 409 else False
     return TypeError("is_409 cannot check item with missing status_code attribute")
