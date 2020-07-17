4540f5725c0c7fd8dc5bf830b5dbc5953a547409
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-18 01:08:33 +1000

    ensure python 3 (helps debugging)

--- gantree_node_watchdog/__init__.py
+++ gantree_node_watchdog/__init__.py
@@ -1,5 +1,11 @@
 """Gantree Node Watchdog - Node monitoring client."""
 
+from sys import version_info
+
+if version_info.major < 3:
+    print("Error: Unsupported Python version. Please use Python 3 or above.")
+    exit()
+
 from .script import main
 
 __version__ = "0.1.1"
