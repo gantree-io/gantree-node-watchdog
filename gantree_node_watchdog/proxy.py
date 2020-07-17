153b5cf1d31d47e29fae7e0d014cec962271c1c7
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-17 15:40:30 +1000

    proxy imports and constants

---
+++ gantree_node_watchdog/proxy.py
@@ -0,0 +1,18 @@
+import requests
+import colorama
+
+from .utils import printStatus, expect200
+
+REGISTER_MESSAGE = (
+    colorama.Fore.LIGHTBLUE_EX + f"Registering node... " + colorama.Style.RESET_ALL
+)
+SCRAPE_MESSAGE = (
+    colorama.Fore.LIGHTYELLOW_EX
+    + f"Waiting for a scrape request from the proxy... "
+    + colorama.Style.RESET_ALL
+)
+METRICS_MESSAGE = (
+    colorama.Fore.LIGHTYELLOW_EX
+    + "Sending metrics to proxy... "
+    + colorama.Style.RESET_ALL
+)
