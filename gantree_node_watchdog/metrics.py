62c223470147adaf57cd0d4c4796267270c483b8
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-17 15:36:31 +1000

    import future stuff, define some constants

--- gantree_node_watchdog/metrics.py
+++ gantree_node_watchdog/metrics.py
@@ -1,1 +1,17 @@
 """Operations related to getting local metrics."""
+
+import requests
+import colorama
+
+from .utils import printStatus, expect200
+from .conditions import is_false
+
+GET_MESSAGE = (
+    colorama.Fore.LIGHTYELLOW_EX + f"Getting metrics... " + colorama.Style.RESET_ALL
+)
+ACCESSIBLE_MESSAGE = (
+    colorama.Fore.LIGHTBLUE_EX
+    + f"Checking local metrics server is online... "
+    + colorama.Style.RESET_ALL
+)
+
