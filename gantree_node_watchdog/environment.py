39c42fcdc30fa348ef91ecdad94aba4436be3b14
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-17 14:32:16 +1000

    host -> proxy_hostname, add metrics_hostname

--- gantree_node_watchdog/environment.py
+++ gantree_node_watchdog/environment.py
@@ -4,7 +4,8 @@ import os
 def get_env_vars():
     """Fetch required environment variables."""
     return {
-        "hostname": os.getenv("GANTREE_NODE_WATCHDOG_HOSTNAME"),
+        "proxy_hostname": os.getenv("GANTREE_NODE_WATCHDOG_PROXY_HOSTNAME"),
+        "metrics_hostname": os.getenv("GANTREE_NODE_WATCHDOG_METRICS_HOSTNAME"),
         "api_key": os.getenv("GANTREE_NODE_WATCHDOG_API_KEY"),
         "project_id": os.getenv("GANTREE_NODE_WATCHDOG_PROJECT_ID"),
         "ip_address": os.getenv("GANTREE_NODE_WATCHDOG_IP_ADDRESS"),
