aa72816920eb753a57b55f02ea44cda88e5986e6
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-24 17:02:36 +1000

    add quick install script

---
+++ quick-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# download latest linux release
+echo "Downloading..."
+curl -s https://api.github.com/repos/gantree-io/gantree-node-watchdog/releases/latest \
+| grep "browser_download_url.*-linux.tar.gz" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+# unpack
+echo "Unpacking..."
+tar -xf ./gantree-node-watchdog-v*.*.*-linux.tar.gz
+
+# execute
+echo "Executing..."
+cd gantree-node-watchdog-v*.*.*-linux && ./bin/gantree_node_watchdog
