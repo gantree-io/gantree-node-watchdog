1f3945d453acc91dc3beb211d5819c5d6ad75122
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-07-24 12:05:08 +1000

    add internal error msg to init for consistency

--- gantree_node_watchdog/__init__.py
+++ gantree_node_watchdog/__init__.py
@@ -6,10 +6,14 @@ if version_info.major < 3:
     print("Error: Unsupported Python version. Please use Python 3 or above.")
     exit()
 
-from .script import main
-
 __version__ = "0.2.0"
 __author__ = "Denver Pallis (DrTexx)"
 __company__ = "Gantree"
 __repository__ = "https://github.com/gantree-io/gantree-node-watchdog"
 __bug_tracker__ = __repository__ + "/issues"
+
+internal_error_message = (
+    f"If you see this message, please open a new issue at {__bug_tracker__}. Thanks!"
+)
+
+from .script import main
