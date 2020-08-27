88891f17d02af6efd859963546bd35491b033735
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-08-27 18:06:42 +1000

    add is_403 condition

--- gantree_node_watchdog/conditions.py
+++ gantree_node_watchdog/conditions.py
@@ -17,6 +17,17 @@ def is_false(item):
     return True if item is False else False
 
 
+def is_403(item):
+    """Return True if the item has a status_code of 403 (forbidden/unauthorized).
+    
+    Return TypeError if item doesn't have a status_code.
+    """
+    if hasattr(item, "status_code"):
+        return (True, "Forbidden") if item.status_code == 403 else False
+
+    return TypeError("is_403 cannot check item with missing status_code attribute")
+
+
 def is_409(item):
     """Return True if the item has a status_code of 409 (conflict).
     
