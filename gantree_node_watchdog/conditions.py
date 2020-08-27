437702b5722c14286fcd7cb1b28840215aa7da0b
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-08-27 18:05:59 +1000

    add extra info to is_409 cond. docstring

--- gantree_node_watchdog/conditions.py
+++ gantree_node_watchdog/conditions.py
@@ -18,7 +18,7 @@ def is_false(item):
 
 
 def is_409(item):
-    """Return True if the item has a status_code of 409.
+    """Return True if the item has a status_code of 409 (conflict).
     
     Return TypeError if item doesn't have a status_code.
     """
